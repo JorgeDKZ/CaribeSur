989c565f567ac4dd68500310b03e1d8ab000d7ef
Jorge Navarro <130685403+JorgeDKZ@users.noreply.github.com>
2025-08-02 13:01:52 +0200

    Update the class plane

--- sur/src/main/java/com/caribe/sur/model/DTO/Plane.java
+++ sur/src/main/java/com/caribe/sur/model/DTO/Plane.java
@@ -1,38 +1,89 @@
 package com.caribe.sur.model.DTO;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 import com.caribe.sur.model.Enumerators.Sites;
 import com.caribe.sur.model.Enumerators.SizeOfPlane;
 
 public class Plane {
-    private int sizeX;
-    private int sizeY;
+    //ATRIBUTES
+    //Places of the plane X = Long of the plane, Y = Place in the both sites
+    private int[][] planeSeats;
 
+    //Site where the plane starts
     private Sites siteStart;
+    //Site where the plane going to end
     private Sites siteEnd;
+    
+    //All seats of the plane
+    private int allSeats;
+    //Available seats of the plane
+    private int availableSeats;
 
+    //Default price of the plane
     private float price;
+    private float priceSelecterSeat;
 
-    public Plane(SizeOfPlane size, Sites siteStart, Sites siteEnd, float price) {
+    //This date is the date when the plane will take off
+    private LocalDate dateOfFlight;
+
+    //CONSTRUCTOR
+    public Plane(SizeOfPlane size, Sites siteStart, Sites siteEnd, float price, float priceSelecterSeat, LocalDate dateOfFlight) {
         generateSize(size);
+        this.allSeats = planeSeats.length * planeSeats[0].length;
+        this.availableSeats = planeSeats.length * planeSeats[0].length;
         this.siteStart = siteStart;
         this.siteEnd = siteEnd;
         this.price = price;
+        this.priceSelecterSeat = priceSelecterSeat;
+        this.dateOfFlight = dateOfFlight;
+        
     }
 
+    //METHODS
+    //Generate the size of the plane
+    //Based on the size of the plane, it will generate the number of seats
     private void generateSize(SizeOfPlane size) {
         switch (size) {
             case SMALL:
-                this.sizeX = 30;
-                this.sizeY = 2;
+                planeSeats = new int[30][2];
                 break;
             case MEDIUM:
-                this.sizeX = 40;
-                this.sizeY = 2;
+                planeSeats = new int[40][2];
                 break;
             case LARGE:
-                this.sizeX = 40;
-                this.sizeY = 3;
+                planeSeats = new int[40][3];
                 break;
+            default:
+                planeSeats = new int[30][2];
         }
-}
+
+    }
+
+    //GETTERS AND SETTERS
+    //Return the price of the plane
+    //If the user select a seat, the price will be increased
+    //If have a few days to the flight, the price will be increased
+    //If the plane is almost full, the price will be increased
+    //If the plane is empty the price will drop
+    public float getPrice(Boolean isSelecterSeat) {
+        float endPrice = (isSelecterSeat) ? priceSelecterSeat + price : price;
+        float AvailablePercentage = getAvailablePorcentage();
+        long day = getDaysToFlight();
+        //Not finish the method
+        return 0;
+
+    }
+
+    // Return the available percentage of the plane
+    private float getAvailablePorcentage() {
+        return availableSeats / allSeats * 100;
+    }
+
+    // Return the days to the flight
+    public long getDaysToFlight() {
+        return ChronoUnit.DAYS.between(LocalDate.now(), dateOfFlight);
+    }
+
 }
